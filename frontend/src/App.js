390d1a4e3ad5b7b63a0288e795db512cbfd1b501
christophersiem <christopher.siem@gmail.com>
2021-06-15 19:50:22 +0200

    Add basic frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
+import {useState} from "react";
+import axios from "axios";
 
 
 function App() {
-  return (
-    <div className="App">
-     <p>Hello App</p>
-    </div>
-  );
+    const [image, setImage] = useState();
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        axios.post("/api/image/upload", image).catch(error => console.error(error.message))
+    }
+    return (
+        <div className="App">
+            <p>Hello App</p>
+            <form onSubmit={handleSubmit}>
+                <input type="file" accept={"image/*"} onChange={event => setImage(event.target.files[0])}/>
+                <button>Upload</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
